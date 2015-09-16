05c27b05277d62fc461d5e3af1e1422fe501deed
unknown <hm2@hm.(none)>
2015-09-16 08:35:37 +0430

    backup5

--- route.py
+++ route.py
@@ -8,11 +8,20 @@ import os
 
 dummyTest=True
 
+#-------------------------------------------
+
 if dummyTest: command='type "'+os.path.dirname(os.path.realpath(__file__))+'\dummy_routes.txt"'
 else: command='route print 0.0.0.0'
 
 p=subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 out,err=p.communicate()
+
+fp=open(os.path.dirname(os.path.realpath(__file__))+'\\routes.log', 'ab')
+fp.write(bytes("\n", 'ascii'))
+fp.write(out)
+fp.write(bytes("\n=============================================================\n", 'ascii'))
+fp.close()
+
 out=out.decode()
 
 result=re.findall('[ \t]+(0.0.0.0)[ \t]+(0.0.0.0)[ \t]+([0-9.]{7,})[ \t]+([0-9.]{7,})[ \t]+([0-9]+)', out)
@@ -129,7 +138,6 @@ class Application(Frame):
 #-------------------------------------------
 		
 root = Tk()
-root.title('Default root')
+root.title('Internet switcher')
 app = Application(master=root)
 app.mainloop()
-
